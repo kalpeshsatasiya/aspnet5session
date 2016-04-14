dafa897c0048c67c56054ab1fc92a2eb33ada2bd
kalpeshsatasiya <satasiya.kalpesh2006@gmail.com>
2016-04-14 22:34:07 +0530

    Session state use with asp.net 5 and MVC 6

---
+++ src/aspnet5session/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Http;
+
+namespace aspnet5session.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ///Set Session values
+            HttpContext.Session.SetInt32("UserId", 1);
+            HttpContext.Session.SetString("UserName", "Kalpesh");
+
+            ///Get Session Values
+            ViewBag.UserId = HttpContext.Session.GetInt32("UserId");
+            ViewBag.UserName = HttpContext.Session.GetString("UserName");
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/aspnet5session/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet5session.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
